f76bdb95722f256aaf379319b980632b6e29618c
Akaifox16 <kamonpat.sun@outlook.com>
2023-01-10 23:27:21 +0700

    remove unused code

--- src/components/Layout/UserBtn.tsx
+++ src/components/Layout/UserBtn.tsx
@@ -1,20 +1,10 @@
-import { MouseEvent, useEffect } from 'react'
 import Link from 'next/link'
 
 import { AccountCircle, Logout } from '@mui/icons-material'
 import {
-	Avatar,
 	Box,
 	Button,
-	Divider,
 	Grid,
-	IconButton,
-	ListItemIcon,
-	Menu,
-	MenuItem,
-	PopoverOrigin,
-	Tooltip,
-	Typography,
 } from '@mui/material'
 import { useSelector } from '@xstate/react'
 import {
@@ -22,71 +12,16 @@ import {
 	useUserMachine,
 } from '@component/Context/AuthContext'
 
-const settings = ['Profile']
-
-type UserBtnProps = {
-	username?: string
-}
-
-const origin: PopoverOrigin = {
-	vertical: 'top',
-	horizontal: 'right',
-}
-const MenuSx = {
-	elevation: 0,
-	sx: {
-		overflow: 'visible',
-		filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-		mt: 1.5,
-		'& .MuiAvatar-root': {
-			width: 32,
-			height: 32,
-			ml: -0.5,
-			mr: 1,
-		},
-		'&:before': {
-			content: '""',
-			display: 'block',
-			position: 'absolute',
-			top: 0,
-			right: 14,
-			width: 10,
-			height: 10,
-			bgcolor: 'background.paper',
-			transform: 'translateY(-50%) rotate(45deg)',
-			zIndex: 0,
-		},
-	},
-}
-
 const UserBtn = () => {
-	// const [anchor, setAnchor] = useState<HTMLElement | null>(null)
 	const userMachine = useUserMachine()
 	const isLoggedIn = useSelector(
 		userMachine.authService,
 		loggedInSelector
 	)
-	// const handleOpenUserMenu = (evt: MouseEvent<HTMLElement>) => {
-	// 	setAnchor(evt.currentTarget)
-	// }
 
-	// const handleCloseUserMenu = () => {
-	// 	setAnchor(null)
-	// }
-	// useEffect(() => {
-	// 	userMachine.authService.send('LOGGED_IN')
-	// }, [])
 
 	return (
 		<Box sx={{ flexGrow: 0 }}>
-			{/* <Tooltip title='Open settings'>
-			</Tooltip> */}
-			{/* <IconButton onClick={handleOpenUserMenu}> */}
-			{/* </IconButton> */}
-			{/* <UserMenu
-				anchor={anchor}
-				handleCloseMenu={handleCloseUserMenu}
-			/> */}
 			{isLoggedIn ? <LogoutBtn /> : <LoginBtn />}
 		</Box>
 	)
@@ -122,44 +57,6 @@ const LogoutBtn = () => {
 				</Link>
 			</Grid>
 		</Grid>
-	)
-}
-
-type UserMenuProps = {
-	anchor: HTMLElement | null
-	handleCloseMenu: () => void
-}
-
-const UserMenu = ({ anchor, handleCloseMenu }: UserMenuProps) => {
-	return (
-		<Menu
-			id='menu-appbar'
-			sx={{ mt: '45px' }}
-			anchorEl={anchor}
-			anchorOrigin={origin}
-			transformOrigin={origin}
-			open={Boolean(anchor)}
-			onClose={handleCloseMenu}
-			PaperProps={MenuSx}
-			keepMounted
-		>
-			{settings.map((option) => (
-				<Link key={option} href={`/${option.toLowerCase()}`}>
-					<MenuItem key={option} onClick={handleCloseMenu}>
-						<Typography>{option}</Typography>
-					</MenuItem>
-				</Link>
-			))}
-			<Divider />
-			<MenuItem key='logout' onClick={handleCloseMenu}>
-				<ListItemIcon>
-					<Logout fontSize='small' color='warning' />
-				</ListItemIcon>
-				<Typography textAlign='center' color='red'>
-					logout
-				</Typography>
-			</MenuItem>
-		</Menu>
 	)
 }
 
