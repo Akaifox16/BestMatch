6f19b8571ea3d4da002285d9c30ba668c786856c
Akaifox16 <kamonpat.sun@outlook.com>
2023-01-11 12:56:56 +0700

    add radio group

---
+++ src/components/Input/RadioGroup.tsx
@@ -0,0 +1,27 @@
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"
+
+type CustomRadioGroupProps = {
+  label: string
+  values: Array<string>
+}
+const CustomRadioGroup = ({label, values}: CustomRadioGroupProps) => {
+  return <FormControl>
+    <FormLabel>{label}</FormLabel>
+    <RadioGroup
+      row
+      name={`${label}-radio-group`}
+    >
+      {
+        values.map(val => {
+          return <FormControlLabel
+            label={val}
+            value={val}
+            control={<Radio />}
+          />
+        })
+      }
+    </RadioGroup>
+  </FormControl>
+}
+
+export default CustomRadioGroup
