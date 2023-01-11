b6f09cd9e9e7eb7e1fecc9b258df017c4f7f160e
Akaifox16 <kamonpat.sun@outlook.com>
2023-01-11 13:34:32 +0700

    remove unused code

--- src/components/ProfileCard/Dorm.tsx
+++ src/components/ProfileCard/Dorm.tsx
@@ -1,32 +1,47 @@
-import RadioGroup from "@component/Input/RadioGroup"
-import { FormControl, FormControlLabel, FormLabel, Grid, Radio, Typography } from "@mui/material"
-import { Fragment } from "react"
+import RadioGroup from '@component/Input/RadioGroup'
+import { Grid, Typography } from '@mui/material'
+import { Fragment } from 'react'
 
 const DormCard = () => {
-  return <Fragment>
-   <RoomPref /> 
-   <DormPref/>
-  </Fragment>
+	return (
+		<Fragment>
+			<RoomPref />
+			<DormPref />
+		</Fragment>
+	)
 }
 
 export default DormCard
 
 const DormPref = () => {
-  return <Fragment>
-    <Typography variant='h5'>Dorm</Typography>
-    <Grid item xs={12}>
-      <RadioGroup label="ค่าหอ" values={['2000', '8000', '5000/เดือน']} />  
-      <RadioGroup label="จำนวนรูมเมท" values={['2', '3', '4']} />  
-    </Grid>
-  </Fragment>
+	return (
+		<Fragment>
+			<Typography variant='h5'>Dorm</Typography>
+			<Grid item xs={12}>
+				<RadioGroup
+					label='ค่าหอ'
+					values={['2000', '8000', '5000/เดือน']}
+				/>
+				<RadioGroup label='จำนวนรูมเมท' values={['2', '3', '4']} />
+			</Grid>
+		</Fragment>
+	)
 }
 
 const RoomPref = () => {
-  return <Fragment>
-    <Typography variant='h5'>Room</Typography>
-    <Grid item xs={12}>
-     <RadioGroup label='โซนห้องพัก' values={['ทะเลทราย', 'ป่าดงดิบ','อะไรก็เอา']}/>
-     <RadioGroup label='ชั้นของห้องพัก' values={['1', '2','3','4','5', 'อะไรก็เอา']}/>
-    </Grid>
-  </Fragment>
+	return (
+		<Fragment>
+			<Typography variant='h5'>Room</Typography>
+			<Grid item xs={12}>
+				<RadioGroup
+					label='โซนห้องพัก'
+					values={['ทะเลทราย', 'ป่าดงดิบ', 'อะไรก็เอา']}
+				/>
+				<RadioGroup
+					label='ชั้นของห้องพัก'
+					values={['1', '2', '3', '4', '5', 'อะไรก็เอา']}
+				/>
+			</Grid>
+		</Fragment>
+	)
 }
