59d820f38cc48aec6ba64b46051dbb3c9dc32676
Akaifox16 <kamonpat.sun@outlook.com>
2023-01-11 12:57:10 +0700

    add dorm preference content

--- src/components/ProfileCard/Dorm.tsx
+++ src/components/ProfileCard/Dorm.tsx
@@ -1,9 +1,32 @@
+import RadioGroup from "@component/Input/RadioGroup"
+import { FormControl, FormControlLabel, FormLabel, Grid, Radio, Typography } from "@mui/material"
 import { Fragment } from "react"
 
 const DormCard = () => {
   return <Fragment>
-
+   <RoomPref /> 
+   <DormPref/>
   </Fragment>
 }
 
 export default DormCard
+
+const DormPref = () => {
+  return <Fragment>
+    <Typography variant='h5'>Dorm</Typography>
+    <Grid item xs={12}>
+      <RadioGroup label="ค่าหอ" values={['2000', '8000', '5000/เดือน']} />  
+      <RadioGroup label="จำนวนรูมเมท" values={['2', '3', '4']} />  
+    </Grid>
+  </Fragment>
+}
+
+const RoomPref = () => {
+  return <Fragment>
+    <Typography variant='h5'>Room</Typography>
+    <Grid item xs={12}>
+     <RadioGroup label='โซนห้องพัก' values={['ทะเลทราย', 'ป่าดงดิบ','อะไรก็เอา']}/>
+     <RadioGroup label='ชั้นของห้องพัก' values={['1', '2','3','4','5', 'อะไรก็เอา']}/>
+    </Grid>
+  </Fragment>
+}
