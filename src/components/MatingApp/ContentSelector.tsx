bdd01a8166db6f9d738cc66a380ce9409bb36b19
Akaifox16 <kamonpat.sun@outlook.com>
2023-01-11 12:27:47 +0700

    split MatingApp

---
+++ src/components/MatingApp/ContentSelector.tsx
@@ -0,0 +1,50 @@
+import { currentStateSelector, useMateStepperMachine } from "@component/Context/MateStepperContext"
+import ProfileCard from "@component/ProfileCard"
+import { MatchVariant } from "@component/ProfileCard/index.type"
+import { Box, Button } from "@mui/material"
+import { useSelector } from "@xstate/react"
+import { Fragment } from "react"
+import { steps } from "."
+
+const ContentSelector = ({
+	activeStep,
+}: {
+	activeStep: number
+}) => {
+
+	const { mateService, plusOne, subOne } = useMateStepperMachine()
+	const currentState = useSelector(
+		mateService,
+		currentStateSelector,
+	)
+	const next = () => {
+		mateService.send({type: 'NEXT'})
+		plusOne()
+	}
+	const back = () => {
+		mateService.send({type: 'BACK'})
+		subOne()
+	}
+
+	return (
+		<Fragment>
+			<ProfileCard variant={currentState as MatchVariant} />
+			<Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+				<Button
+					color='inherit'
+					disabled={activeStep === 0}
+					onClick={back}
+					sx={{ mr: 1 }}
+				>
+					กลับ
+				</Button>
+				<Box sx={{ flex: '1 1 auto' }} />
+				<Button onClick={next}>
+					{activeStep === steps.length - 1 ? 'ไปจับคู่กัน' : 'ต่อไป'}
+				</Button>
+			</Box>
+		</Fragment>
+	)
+}
+
+export default ContentSelector
--- src/components/MatingApp/index.tsx
+++ src/components/MatingApp/index.tsx
@@ -1,22 +1,16 @@
 import {
-	currentStateSelector,
 	MateStepperProvider,
 	useMateStepperMachine,
 } from '@component/Context/MateStepperContext'
-import ProfileCard from '@component/ProfileCard'
-import { MatchVariant } from '@component/ProfileCard/index.type'
 import {
-	Box,
-	Button,
 	Stack,
 	Step,
 	StepLabel,
 	Stepper,
 } from '@mui/material'
-import { useSelector } from '@xstate/react'
-import { Fragment } from 'react'
+import ContentSelector from './ContentSelector'
 
-const steps = [
+export const steps = [
 	'ระบุลักษณะของคุณ',
 	'ระบุลักษณะรูมเมทที่คุณอยากได้',
 	'ระบุลักษณะห้องพักที่คุณอยากได้',
@@ -41,48 +35,7 @@ const MatingApp = () => {
 	)
 }
 
-const ContentSelector = ({
-	activeStep,
-}: {
-	activeStep: number
-}) => {
-
-	const { mateService, plusOne, subOne } = useMateStepperMachine()
-	const currentState = useSelector(
-		mateService,
-		currentStateSelector,
-	)
-	const next = () => {
-		mateService.send({type: 'NEXT'})
-		plusOne()
-	}
-	const back = () => {
-		mateService.send({type: 'BACK'})
-		subOne()
-	}
-
-	return (
-		<Fragment>
-			<ProfileCard variant={currentState as MatchVariant} />
-			<Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-				<Button
-					color='inherit'
-					disabled={activeStep === 0}
-					onClick={back}
-					sx={{ mr: 1 }}
-				>
-					กลับ
-				</Button>
-				<Box sx={{ flex: '1 1 auto' }} />
-				<Button onClick={next}>
-					{activeStep === steps.length - 1 ? 'ไปจับคู่กัน' : 'ต่อไป'}
-				</Button>
-			</Box>
-		</Fragment>
-	)
-}
-
-const MatingAppWrap = () => {
+const MatingAppWrapper = () => {
 	return (
 		<MateStepperProvider>
 			<MatingApp />
@@ -90,4 +43,4 @@ const MatingAppWrap = () => {
 	)
 }
 
-export default MatingAppWrap
+export default MatingAppWrapper
