548da169e900a7b9c9e8c86d774534c587b74491
Akaifox16 <kamonpat.sun@outlook.com>
2023-01-18 21:11:25 +0700

    change user to student

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -2,7 +2,7 @@ import { router } from "server/trpc";
 import { userRouter } from './user';
 
 export const appRouter = router({
-  user: userRouter,
+  student: userRouter,
 })
 
 export type AppRouter = typeof appRouter
