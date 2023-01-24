5e427f38b91c4f37f0d32b80363b2b975fe4c602
Akaifox16 <kamonpat.sun@outlook.com>
2023-01-24 17:26:00 +0700

    add mockProfile implementation

--- src/server/controller/match/mockProfile.ts
+++ src/server/controller/match/mockProfile.ts
@@ -1,9 +1,49 @@
 import { publicProcedure } from '@server/trpc'
+import { prisma } from '@server/db'
+import { NotFoundError } from '@server/model/errors'
 
 const mockProfile = publicProcedure.query(async () => {
-	return {
-		message: 'need implementation',
+	const userId = 'cld1ns6f80000qt54q7jf81ej'
+
+	const matePref = await prisma.profile.findFirst({
+		where: {
+			pref_owner_id: userId,
+		},
+		select: {
+			messiness: true,
+			loudness: true,
+			do_not_disturb: {
+				select: {
+					start: true,
+					stop: true,
+				},
+			},
+		},
+	})
+
+	if (!matePref) {
+		throw NotFoundError('sorry, we did\'t found your roommate preference')
 	}
+
+	const generatedProfile: typeof matePref[] = [
+		{ ...matePref, messiness: matePref.messiness + 1 },
+		{ ...matePref, loudness: matePref.loudness + 1 },
+		{
+			...matePref,
+			do_not_disturb: matePref.do_not_disturb.map((range) => {
+				return {
+					start: new Date(
+						range.start.setHours(range.start.getHours() + 1)
+					),
+					stop: new Date(
+						range.stop.setHours(range.stop.getHours() + 1)
+					),
+				}
+			}),
+		},
+	]
+
+	return generatedProfile
 })
 
 export default mockProfile
