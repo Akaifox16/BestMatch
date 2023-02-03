c5e1e3ac07c21eed7cbc68e9358f5a9ca1530fa5
Akaifox16 <kamonpat.sun@outlook.com>
2023-02-03 10:56:31 +0700

    change @bm to @acme

--- apps/student/next.config.mjs
+++ apps/student/next.config.mjs
@@ -1,13 +1,13 @@
-!process.env.SKIP_ENV_VALIDATION && (await import('@bm/env'))
+!process.env.SKIP_ENV_VALIDATION && (await import('@acme/env'))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	},
-	transpilePackages: ['@bm/server'],
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  transpilePackages: ['@bm/server'],
 }
 
 export default nextConfig
