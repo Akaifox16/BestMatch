9451641823e546a820563e89f73e8a09bb215f34
Akaifox16 <kamonpat.sun@outlook.com>
2023-02-14 23:20:21 +0700

    add range input with preview

--- apps/student/src/components/Input/RangeWithPreview.tsx
+++ apps/student/src/components/Input/RangeWithPreview.tsx
@@ -1,5 +1,23 @@
-import { Fragment } from 'react';
+import {
+  FieldValues,
+  SliderElement,
+  SliderElementProps,
+} from 'react-hook-form-mui';
 
-export default function RangeWithPreview() {
-  return <Fragment></Fragment>;
+export default function RangeWithPreview<Control extends FieldValues>({
+  control,
+  name,
+  label,
+}: Pick<SliderElementProps<Control>, 'name' | 'label' | 'control'>) {
+  return (
+    <SliderElement
+      control={control}
+      name={name}
+      label={label}
+      max={9}
+      min={1}
+      marks
+      required
+    />
+  );
 }
