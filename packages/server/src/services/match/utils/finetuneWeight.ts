13a848896d14e3c85e19a8422025f552f2343df8
Akaifox16 <kamonpat.sun@outlook.com>
2023-03-01 21:48:38 +0700

    BM-8 impl renewWeight

--- packages/server/src/services/match/utils/finetuneWeight.ts
+++ packages/server/src/services/match/utils/finetuneWeight.ts
@@ -1,43 +1,46 @@
-import { type CalculatedPreference, prisma } from '@acme/database';
+import {
+  prisma,
+  type CalculatedPreference,
+  type DoNotDisturbTolerant,
+} from '@acme/database';
 import type { ProfileAttributes } from '../../../utils/type';
 
 type Input = {
   penalty: number;
   attr: ProfileAttributes;
 };
-
-type RenewWeightProfile = Pick<
-  CalculatedPreference,
-  'do_not_disturb_weight' | 'loudness_weight' | 'messiness_weight'
+type RenewWeightProfile = Partial<
+  Pick<
+    CalculatedPreference,
+    'do_not_disturb_weight' | 'loudness_weight' | 'messiness_weight'
+  >
 >;
+type CalculatedPreferenceWithDoNotDisturb = CalculatedPreference & {
+  do_not_disturb_tolerant: DoNotDisturbTolerant;
+};
 
 type Tolerant = {
   max: number;
   min: number;
 };
-
-// TODO: impl weightNormalization
-function weightNormalization(weight: number, tolerant: Tolerant): number {
-  return -1;
-}
-
-// TODO: impl renewWeight
-function renewWeight(
-  attr: ProfileAttributes,
-  normalizedNewWeight: number
-): RenewWeightProfile {
-  return {} as RenewWeightProfile;
-}
+type NormalizationInput = {
+  weight: number;
+  tolerant: Tolerant;
+};
 
 // TODO: BM-8 | implement how to find student preference priority
 export async function finetuneWeight(
   select: Input,
   comparer: Input,
-  profile: CalculatedPreference
+  profile: CalculatedPreferenceWithDoNotDisturb
 ) {
-  const newWeight = calculateNewWeight();
-  const normalizedNewWeight = weightNormalization(newWeight);
+  const selected_tolerant = getTolerant(select.attr, profile);
+  const comparer_tolerant = getTolerant(comparer.attr, profile);
 
+  const normalizedNewWeight = weightNormalization(
+    selected_tolerant,
+    comparer_tolerant
+  );
   const newProfile = renewWeight(select.attr, normalizedNewWeight);
 
   try {
@@ -52,4 +55,65 @@ export async function finetuneWeight(
   } catch (e) {
     console.error(`Error: cannot update your preference profile`);
   }
+}
+
+// TODO: impl weightNormalization
+function weightNormalization(
+  selected: NormalizationInput,
+  comparer: NormalizationInput
+) {
+  // const
+  return -1;
+}
+
+// TODO: impl renewWeight
+function renewWeight(
+  attr: ProfileAttributes,
+  normalizedNewWeight: number
+): RenewWeightProfile {
+  switch (attr) {
+    case 'messiness':
+      return {
+        messiness_weight: normalizedNewWeight,
+      };
+    case 'loudness':
+      return {
+        loudness_weight: normalizedNewWeight,
+      };
+    case 'do_not_disturb':
+      return {
+        do_not_disturb_weight: normalizedNewWeight,
+      };
+    default:
+      throw new Error('receive unexpected attribute');
+  }
+}
+
+function getTolerant(
+  attr: ProfileAttributes,
+  profile: CalculatedPreferenceWithDoNotDisturb
+): NormalizationInput {
+  return attr === 'messiness'
+    ? {
+        tolerant: {
+          min: profile.messiness_tolerant_min,
+          max: profile.messiness_tolerant_max,
+        },
+        weight: profile.messiness_weight,
+      }
+    : attr === 'loudness'
+    ? {
+        tolerant: {
+          min: profile.loudness_tolerant_min,
+          max: profile.loudness_tolerant_max,
+        },
+        weight: profile.loudness_weight,
+      }
+    : {
+        tolerant: {
+          min: profile.do_not_disturb_tolerant.start_min,
+          max: profile.do_not_disturb_tolerant.stop_max,
+        },
+        weight: profile.do_not_disturb_weight,
+      };
 }
