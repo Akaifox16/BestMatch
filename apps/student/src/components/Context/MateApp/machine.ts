2ea10183db030fcc8853e10c4d771454dbda700c
Akaifox16 <kamonpat.sun@outlook.com>
2023-03-02 23:30:01 +0700

    BM-77 add actions and guard

--- apps/student/src/components/Context/MateApp/machine.ts
+++ apps/student/src/components/Context/MateApp/machine.ts
@@ -1,35 +1,50 @@
-import { createMachine } from "xstate";
-import { finetunerMachine } from "../Finetuner";
-import mateStepperMachine from "../MateStepper/machine";
+import { assign, createMachine } from 'xstate';
+import { finetunerMachine } from '../Finetuner';
+import mateStepperMachine from '../MateStepper/machine';
 
-const mateAppMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGAXMB9VAHXWaAxgBYCWAdmAHQCuFA1hQPYDuFAxAJIByXAKgG0ADAF1EoXM1hl0ZZhQkgAHogC0AJgDsAZmoAWAIwA2Yzp3HhAVgCcVrcJ0AaEAE9EOrRuo3Dh-cbaABw6QYY6VgC+kS5omDj4hKiklDT0TGycvAKChuJIIFIycgpKqgiaQcLUGsLC+kFWGho2+jbNLu4IVm3UDiH6PeG1QdGxGNh4BMTkVNRxYADKmPhgAE4cEAo0lABuzAw0C8tgq2uL67tkRGAi+ZLSsvKKBeVqJtQ6JhZhjsLaOn0+k6iCsVkMNUMzXBph0I2iMRALAgcCUCwS02SszASiKT1Kr3U+l0Bj8QKsxkMWmMWisQRBCA0QW8QX0nkpQP0wl8o0R6KmSRSc3SLHYuMeJReoDe+g0VlJ-kGlOptPpbg8XmoQRsrKMXwCNi8WjGIH5iRmqWoADNUuh6OtxcVnmVEH5qIZwcJAsI-FpDEFaQzjE0tc0gsZBnDicJjXyJhjBdj5hMTmdHfipSpXcYtf8oVyNIDPFYGZ5qnCWnKKdSNMY7AjIkA */
-  id: 'mate_app_machine',
-  context: { initialized: false},
-  initial: 'unknown',
-  states:{
-    unknown: {
-      on: {
-        INIT: [{
-          target: "mateStepper",
-          cond: "isInitialize"
-        }, "finetuner"]
-      }
+const mateAppMachine = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5QFsCGAXMB9VAHXWaAxgBYCWAdmAHQCuFA1hQPYDuFAxAJIByXAKgG0ADAF1EoXM1hl0ZZhQkgAHogC0AJgDsAZmoAWAIwA2Yzp3HhAVgCcVrcJ0AaEAE9EOrRuo3Dh-cbaABw6QYY6VgC+kS5omDj4hKiklDT0TGycvAKChuJIIFIycgpKqgiaQcLUGsLC+kFWGho2+jbNLu4IVm3UDiH6PeG1QdGxGNh4BMTkVNRxYADKmPhgAE4cEAo0lABuzAw0C8tgq2uL67tkRGAi+ZLSsvKKBeVqJtQ6JhZhjsLaOn0+k6iCsVkMNUMzXBph0I2iMRALAgcCUCwS02SszASiKT1Kr3U+l0Bj8QKsxkMWmMWisQRBCA0QW8QX0nkpQP0wl8o0R6KmSRSc3SLHYuMeJReoDe+g0VlJ-kGlOptPpbg8XmoQRsrKMXwCNi8WjGIH5iRmqWoADNUuh6OtxcVnmVEH5qIZwcJAsI-FpDEFaQzjE0tc0gsZBnDicJjXyJhjBdj5hMTmdHfipSpXcYtf8oVyNIDPFYGZ5qnCWnKKdSNMY7AjIkA */
+    id: 'mate_app_machine',
+    context: { initialized: false },
+    initial: 'unknown',
+    states: {
+      unknown: {
+        on: {
+          INIT: [
+            {
+              target: 'mateStepper',
+              cond: 'isInitialize',
+            },
+            'finetuner',
+          ],
+        },
+      },
+      finetuner: {
+        invoke: {
+          id: 'finetunerService',
+          src: finetunerMachine,
+        },
+      },
+      mateStepper: {
+        invoke: {
+          id: 'mateStepperService',
+          src: mateStepperMachine,
+          onDone: {
+            target: 'finetuner',
+            actions: assign({
+              initialized: true,
+            }),
+          },
+        },
+      },
     },
-    finetuner: {
-      invoke: {
-        id: 'finetunerService',
-        src: finetunerMachine
-      }
+  },
+  {
+    guards: {
+      isInitialize: (ctx) => ctx.initialized,
     },
-    mateStepper: {
-      invoke: {
-        id: 'mateStepperService',
-        src: mateStepperMachine,
-        onDone: "finetuner"
-      }
-    }
   }
-})
+);
 
-export default mateAppMachine
+export default mateAppMachine;
