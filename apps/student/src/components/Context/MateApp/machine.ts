343e4a1d236224ca6a3975a08728b6a3d1c7c0be
Akaifox16 <kamonpat.sun@outlook.com>
2023-03-10 00:18:31 +0700

    remove form management state machine

--- apps/student/src/components/Context/MateApp/machine.ts
+++ apps/student/src/components/Context/MateApp/machine.ts
@@ -9,13 +9,13 @@ type DormForm = RouterInputs['student']['upsertDormPreference'];
 
 type Events =
       | { type: 'PICKED'; data: { profilePick: Profile; profileComp: Profile } }
-      // | { type: 'NEXT'; data: Control<ProfileForm>}
+      | { type: 'NEXT' }
       | { type: 'PREV' }
-      | { type: 'SUBMIT'; data: Control<DormForm> }
-      | { type: 'Form Input Changed'}
+      // | { type: 'SUBMIT'; data: Control<DormForm> }
+      // | { type: 'Form Input Changed'}
 
 const mateAppMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGAXMB9VAHXWaAxgBYCWAdmAHQCuFA1hQPYDuFAxANoAMAuolC5msMujLMKgkAA9EANgCs1AIwB2eQA5NAZkWKAnDx4AWRWoA0IAJ6I1J6jx0AmNQZ09nOtYpMBfPys0TBx8QlRSShp6JjZOLhUBJBBhUXFJaTkEeQcTHRMtTTVnHk0S5wMrWwRFTWVdFRUTExVjNv9AkGDsPAJicipqADMo9HowACdqABtmVAhKKA4ISRpKADdmBhoJsBgqCYxsXAnmEemwXiShETEJKWSszRMDagM8lQNtTz0vqsQVBVnG88qYNGpPHVFAEgkdQn0IgMaCMqGMDjM5gsKEtJqcprhphghswJshqLt9pM4SczmQLldpKk7hlHohvDpqBUDAYVDoDGo1Doij5-ghGs5lPINPIKrohfzNDCunDeuFIoMUWA0ZMMfNFhxcSTqASiSSyRSwAdqadzpdEozbukHqAsiYStR5N4VIo9J8mm5Rdy1NRig18q4fdyld14WqkcNRuMprASGwOAAFACSAGEANIAUQAIgzkkynZkATwvtRDJonKUCvItDpRU18iGIS0+e5Gjpoyqwv0ognUUnqJTDpgAASG3YQDgyWDoI7UVBDTATAAUrWMAEoODHVUONYn0ROjjOJnjICWbml7hXsvIVNQXrXfCYivkVK38hz3hK-JuhUig8Go-YhEeiLDt0ADKmD4DqsBgNMQzpjadJgBwABiZpTpmFC4LQ6BTtmJCoNiN78A694si6iB5MCoGtB+TglEKiiiiYYGOLyGgmD4RSaJ60KdIeg7QYMcEIbgSEoWhGEXNQKZsIsU7EqSHCwQAqgAQgAspmAAqt4pI6D6sggGgvuYPCfDkUoynyXHVh4WhfCo8hgXKEE9BJ6o0NJYCIcm8nobSSmwag6xqeFtrLKs1AbFsgUDgiAXUEFIXKWFik0FFMXYlOcWYQgyVEBg9xXKZZYWfRCAGIoL7go0ZRgh4ziit4PDUHyrjOJ69jON6fZiWlcYwUc8HBbJoWoSVkXRbFeUGleRomugGlkuJ6Xxlls05fNeXUAVy0RWAZUUJsFVOtV1GluZdGyHYnmvtxPrAZ+nyVDYbK8r1Tg6J5TlgfkHSwpB-l7VNMk6qczDIN06FgEMkyWkQWHpgASvmABqNWPc6z1WXWb3PIU8gKgY8itq4r68tyEo-HyvmxseqWYNN2Xw4jRzI6juwUBjOF4QRREkWRFEwBABO0UTWSNNT1DPDy4q1N6NO-Qg3HBnZ7KuM8zTqODyqQ7tk2c7DUw80juwC+jNAqawalbVpemGSZ913sy8sAooTaOK4LjuAJw2lC5mgA5Tnl1K0ui+KzUEZftcPMAjtso2jQv5UtRVY2nvPTvzWfCysgzJdsmXjezVeWzNqfp3zdslznhVQFO+eN0XzeCxjl3XZVkh3dcZly4+KjPMoAlCjKFRVi8lhazoAcGM43wDZ4TUCSbO0TVJMP19bBcZ-b2cnbn7ed4XYDFT3DurXixqEptZq1355v73X3PH03me963akr7dFvn-B2-dmA3SqvwWWPtx4+h6hPew7h3g8C8jwTiS9-oeGDm4FwAogaiQhu-PeHMwBcwOisUkxd-4ZhxvjL2o9YGWU8qvXq-t7BqGEhPIGLYtaAmDC0binkwLNEZs4ROUMLZkKttQShyBqH31wqSfChFiKkXIpRGWDDapPSyCUAUNYvj+35IoWUwkuI8T1j4CogIzD2EIabYhNcU5TDkQos+TsXZmjdgZYyMDyyWWcIxN4VY1BNF5J5AoEco48i0E1Uoegd7V0kqQ8hOo3F3zPqdIqhY8LuNLolCupC2YpLfmk1xZp8kAJyXkzJfdyqDwoMPGiTD6pBLcDWewiTKbOAqLw6oZQXwlDqMNECy87ISI-qkmRGTQFZIvlOXJyiqkP3Ws-Lab8SnJwPtlWZp8Mbnzbos2pcz6lXQgY05pD0x6BKFJHTQgI3K9O8LUTqmCer6FMd4SmLwtAdE6CwCAcBpC73Zi0gJ9VXp5AKNoYopRyg-WqAAWnKL1JQvzzDGJ9OIsaZsSF0EYCwdg4K6rEzKL1R5dlOHLyKDkUUSLGgOG0DKWJ7gmpr0mfizU2oJgkt0WyCUnJuQ8j5Pg4UGDqhGAEcy5eAkfAeAcaC0p3KxyzD1NiPlvtsifA9J4VwjUWXoMRQCPIrxl4uG9CUHp4FcVOOVaeJCqZWCasfEYHqOgPWAnUO8TQ7wJUAk+MCASphPU8N+ZymuKqzyWipNOWckAXWWQKMGJsxhQJ6CUKvCUtMDFViBMNQwTgygRvtaOdEs4pzTDIMgMQM4ZAY0gAm65rSyU+DeDYsJRgzC6DeZK94bwpTaGpi0GUugS3bK-rNRN9UPXyBrHZdBzQ2JFv9QgPkbx8icI0CUDWn4kl4ucTsg6yEjrnWncTIUDhmKLu4i4Fdopen1CCcIvVdZPQ4qIVs6Gk65KnttMpJ1XjSTnqyM+GyLF47oIhL00UpjlAGvcKg7Quh3Dju-dIw+h0FLnUOWdW0IG7CeE5F5Z41jjaCi6lKQOE9MWNCUB+xxX6pHlPJD-TAVSCNiiaPcl4jRATq39g+wVZRBG8hcKBLyo1P1J3Qyxm2v99mO0A0VLanHeTPl6s8IU-GPDcU1tUODbxjGAmpsDX1aHmMyPk+xup1TL5sZvhx5tELiYjWDBKGem6IQQh-EvKjngaM+Do6Yizn8MO7MqbZzjCLDF1EptYr45itbPMcBoUorR3g+ZcKF6ZmG9ktwA6pFTZposDRsr0looF0sUa1oZhDHrSjU0njlspMzIunLs8c5ZUXnOkr0UKDkn4cijIGo+1d3yQmPt9WvFwMp5ABACEAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGAXMB9VAHXWaAxgBYCWAdmAHQCuFA1hQPYDuFAxANoAMAuolC5msMujLMKgkAA9EANgCs1AIwB2eQA5NAZkWKAnDx4AWRWoA0IAJ6I1J6jx0AmNQZ09nOtYpMBfPys0TBx8QlRSShp6JjZOLhUBJBBhUXFJaTkEeQcTHRMtTTVnHk0S5wMrWwRFTWVdFRUTExVjNv9AkGDsPAJicipqADMo9HowACdqABtmVAhKKA4ISRpKADdmBhoJsBgqCYxsXAnmEemwXiShETEJKWSszWbqMvtnFUUtJTVLG0QDF9qDoVF4fM5DMVNPIAkEjqE+hEBjQRlQxgcZnMFhQlpNTlNcNMMENmBNkNRdvtJvCTmcyBcrtJUncMo9EDotKpnioOfYOXV5FU7JoDI4zID3jp8qVYV14b1wpFBqiwOjJpj5osOHjSdRCcTSeTKWADjTTudLokmbd0g9QFkWspFB9vPJnHVmuYTELsvIVI4DBoWjwVPIDLpZd0EYrkcNRuMprASGwOAAFACSAGEANIAUQAIozkszbZlEK13NQTGUIRzQQZ5H6fRXNKodIHHWV9IZI-Kwv0onG0QnqFTDpgAAQ63YQDgyWDoI7UVBDTATAAUrWMAEoOFGFQPlfGMWOjlOJvjIEWbml7mXskCdJpPKUPoonM5m8Y1MDGgYISGXjuJovYhAeSKDt0ADKmD4OqpzMMg3SprsQyTCaRBgGmABKuYAGrXikNp3myNTVtQfp+r4eheMYKjNtCYrFB8JhvkoeigT0-YQYM0Gwbg6orGSKFgGhuwUJhOH4YRJYkfa5ags4VZ5NWWgQv+OQMfITHOCxbGKBxnT7txSo0HxYBwYmYDTEMKF0hcHAAHK5gAGgAKjJxGsvJNTvq8oZhryahNComg+vkrY+MFnggmGmitIonHRoeZlHDBFkCVMCFIUcIliRhWHOe5nm3t5sgKc4Dg5CYpgGA2zRqDwijheRUWlHkeihk+SXgaZ1DmZZ1BCcgeXoRJhWuR5-DWqVdrlQgnytNQ7p5KFvgaI2Og+hU-ryE+tQNoo6jPh0cJgSZsYDZlyyrNQC5LsZiJ9VdkwlSyc1ZJ8NXUBouhOM+XguJU-zZE11ANiCajPM0YY5AEnQsBAcDSI9MZRDN733qGVb5IUxSvp4wPVAAtOUwJKM0pTFO+rhqD1F2DjELDsBjpakWUv60cd-JBj6xONA42hugYobuEd7r009sYqmqEys3J80uDwXItLyeRaF8PrmMrdRlE4dVfCKOiS2jR7Dhisyaji8tlVkfq5NCNNAyU3jNq0SlqC4viuBDjUqCbKVDqqI5JmwNsfYgIrKB8nuen6Kj-t6IN1PU7YaDw+NuK4Ac8Six7qqek7TpA4f3gUSm+KxnuKSKIY+gY33l5VHbPPFzg531MsjtOE7TGQyBiFOMiYZAJfFl5EcIEUOs8u2BSNdo9Eg0orZSsU6heK0wXG0ZfZS5BaX8ZMpekR4SkhvkLT-mvpiCiD4bUIbTV6LoYaNIlu-nfvvGHxl6qwNZWy5p6RgBPj5PQysL7NAToDRqBRtr-nJn6EMzot7PBhJ-Li39UqYHSoNbKyFUJjUwmA+az5-S6Chq0PIIYPQIKUhyZBnwPgZ3QR3S6v9BrDVGuJEh49ZpYylA4WOzwQQ1QaGFEG7pRQiiMDFViBRGynTlF-U2OCwB4MyndWgAAjAe6BMAQFIZ9EwItgRQ1KPIHw8dfg+hqj+Kx8hSjhhKNzHeAQgA */
   id: 'mate_app_machine',
   tsTypes: {} as import('./machine.typegen').Typegen0,
   context: {
@@ -93,32 +93,11 @@ const mateAppMachine = createMachine({
       states: {
         selfProfile: {
           on: {
-            "Form Input Changed": {
-              target: ".showing form",
-              actions: "assignProfileFormInputToCtx"
+            NEXT: {
+              target: "roommatePreference",
+              actions: "incrementStep"
             }
-          },
-
-          states: {
-            "showing form": {
-              on: {
-                SUBMIT: "Saving Profile"
-              }
-            },
-
-            "Saving Profile": {
-              invoke: {
-                src: "saveProfile",
-                onDone: {
-                  target: "#mate_app_machine.mateStepper.roommatePreference",
-                  actions: "incrementStep"
-                },
-                onError: "showing form"
-              }
-            }
-          },
-
-          initial: "showing form"
+          }
         },
 
         roommatePreference: {
@@ -128,32 +107,11 @@ const mateAppMachine = createMachine({
               actions: 'decrementStep',
             },
 
-            "Form Input Changed": {
-              target: ".showing form",
-              actions: "assignMatePrefFormInputToCtx"
+            NEXT: {
+              target: "dormPreference",
+              actions: "incrementStep"
             }
-          },
-
-          states: {
-            "showing form": {
-              on: {
-                SUBMIT: "Saving Roommate Preference"
-              }
-            },
-
-            "Saving Roommate Preference": {
-              invoke: {
-                src: "saveMatePref",
-                onDone: {
-                  target: "#mate_app_machine.mateStepper.dormPreference",
-                  actions: "incrementStep"
-                },
-                onError: "showing form"
-              }
-            }
-          },
-
-          initial: "showing form"
+          }
         },
 
         dormPreference: {
@@ -163,32 +121,17 @@ const mateAppMachine = createMachine({
               actions: 'decrementStep',
             },
 
-            "Form Input Changed": {
-              target: ".showing form",
-              actions: "assignDormPrefFormInputToCtx"
-            }
-          },
+            NEXT: "submitted"
+          }
+        },
 
-          states: {
-            "showing form": {
-              on: {
-                SUBMIT: "Saving Dorm Preference"
-              }
-            },
-
-            "Saving Dorm Preference": {
-              invoke: {
-                src: "saveDormPref",
-                onDone: "#mate_app_machine.finetuner",
-                onError: "showing form"
-              }
-            }
-          },
-
-          initial: "showing form"
+        submitted: {
+          type: "final"
         }
-      }
-    },
+      },
+
+      onDone: "finetuner"
+    }
   },
   schema: {
     events: {} as Events,
--- apps/student/src/components/Context/MateApp/machine.typegen.ts
+++ apps/student/src/components/Context/MateApp/machine.typegen.ts
@@ -5,9 +5,6 @@
         '@@xstate/typegen': true;
         internalEvents: {
           "": { type: "" };
-"done.invoke.mate_app_machine.mateStepper.dormPreference.Saving Dorm Preference:invocation[0]": { type: "done.invoke.mate_app_machine.mateStepper.dormPreference.Saving Dorm Preference:invocation[0]"; data: unknown; __tip: "See the XState TS docs to learn how to strongly type this." };
-"done.invoke.mate_app_machine.mateStepper.roommatePreference.Saving Roommate Preference:invocation[0]": { type: "done.invoke.mate_app_machine.mateStepper.roommatePreference.Saving Roommate Preference:invocation[0]"; data: unknown; __tip: "See the XState TS docs to learn how to strongly type this." };
-"done.invoke.mate_app_machine.mateStepper.selfProfile.Saving Profile:invocation[0]": { type: "done.invoke.mate_app_machine.mateStepper.selfProfile.Saving Profile:invocation[0]"; data: unknown; __tip: "See the XState TS docs to learn how to strongly type this." };
 "done.invoke.regenerate_profile": { type: "done.invoke.regenerate_profile"; data: unknown; __tip: "See the XState TS docs to learn how to strongly type this." };
 "error.platform.regenerate_profile": { type: "error.platform.regenerate_profile"; data: unknown };
 "xstate.after(1000)#mate_app_machine.finetuner.generate errored": { type: "xstate.after(1000)#mate_app_machine.finetuner.generate errored" };
@@ -15,24 +12,18 @@
         };
         invokeSrcNameMap: {
           "regenerateProfile": "done.invoke.regenerate_profile";
-"saveDormPref": "done.invoke.mate_app_machine.mateStepper.dormPreference.Saving Dorm Preference:invocation[0]";
-"saveMatePref": "done.invoke.mate_app_machine.mateStepper.roommatePreference.Saving Roommate Preference:invocation[0]";
-"saveProfile": "done.invoke.mate_app_machine.mateStepper.selfProfile.Saving Profile:invocation[0]";
         };
         missingImplementations: {
-          actions: "assignDormPrefFormInputToCtx" | "assignMatePrefFormInputToCtx" | "assignProfileFormInputToCtx" | "clearErrorCount" | "decrementStep" | "incrementErrorCount" | "incrementStep" | "pickProfile" | "updateGeneratedProfile";
+          actions: "clearErrorCount" | "decrementStep" | "incrementErrorCount" | "incrementStep" | "pickProfile" | "updateGeneratedProfile";
           delays: never;
           guards: "isInitialize" | "notExceedErrorLimitCount";
-          services: "regenerateProfile" | "saveDormPref" | "saveMatePref" | "saveProfile";
+          services: "regenerateProfile";
         };
         eventsCausingActions: {
-          "assignDormPrefFormInputToCtx": "Form Input Changed";
-"assignMatePrefFormInputToCtx": "Form Input Changed";
-"assignProfileFormInputToCtx": "Form Input Changed";
-"clearErrorCount": "done.invoke.regenerate_profile";
+          "clearErrorCount": "done.invoke.regenerate_profile";
 "decrementStep": "PREV";
 "incrementErrorCount": "error.platform.regenerate_profile";
-"incrementStep": "done.invoke.mate_app_machine.mateStepper.roommatePreference.Saving Roommate Preference:invocation[0]" | "done.invoke.mate_app_machine.mateStepper.selfProfile.Saving Profile:invocation[0]";
+"incrementStep": "NEXT";
 "pickProfile": "PICKED";
 "updateGeneratedProfile": "done.invoke.regenerate_profile";
         };
@@ -44,15 +35,10 @@
 "notExceedErrorLimitCount": "error.platform.regenerate_profile";
         };
         eventsCausingServices: {
-          "regenerateProfile": "" | "PICKED" | "done.invoke.mate_app_machine.mateStepper.dormPreference.Saving Dorm Preference:invocation[0]" | "xstate.after(1000)#mate_app_machine.finetuner.generate errored";
-"saveDormPref": "SUBMIT";
-"saveMatePref": "SUBMIT";
-"saveProfile": "SUBMIT";
+          "regenerateProfile": "" | "PICKED" | "done.state.mate_app_machine.mateStepper" | "xstate.after(1000)#mate_app_machine.finetuner.generate errored";
         };
-        matchesStates: "finetuner" | "finetuner.error limit exceeded" | "finetuner.generate errored" | "finetuner.loading" | "finetuner.show" | "mateStepper" | "mateStepper.dormPreference" | "mateStepper.dormPreference.Saving Dorm Preference" | "mateStepper.dormPreference.showing form" | "mateStepper.roommatePreference" | "mateStepper.roommatePreference.Saving Roommate Preference" | "mateStepper.roommatePreference.showing form" | "mateStepper.selfProfile" | "mateStepper.selfProfile.Saving Profile" | "mateStepper.selfProfile.showing form" | "unknown" | { "finetuner"?: "error limit exceeded" | "generate errored" | "loading" | "show";
-"mateStepper"?: "dormPreference" | "roommatePreference" | "selfProfile" | { "dormPreference"?: "Saving Dorm Preference" | "showing form";
-"roommatePreference"?: "Saving Roommate Preference" | "showing form";
-"selfProfile"?: "Saving Profile" | "showing form"; }; };
+        matchesStates: "finetuner" | "finetuner.error limit exceeded" | "finetuner.generate errored" | "finetuner.loading" | "finetuner.show" | "mateStepper" | "mateStepper.dormPreference" | "mateStepper.roommatePreference" | "mateStepper.selfProfile" | "mateStepper.submitted" | "unknown" | { "finetuner"?: "error limit exceeded" | "generate errored" | "loading" | "show";
+"mateStepper"?: "dormPreference" | "roommatePreference" | "selfProfile" | "submitted"; };
         tags: never;
       }
   
