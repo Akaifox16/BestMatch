7c7b896790d891270d7bc48cfaeb1e2b28fc3e92
Akaifox16 <kamonpat.sun@outlook.com>
2023-03-31 17:26:18 +0700

    add docker compose

--- apps/student/next.config.mjs
+++ apps/student/next.config.mjs
@@ -1,7 +1,14 @@
 !process.env.SKIP_ENV_VALIDATION && (await import('@acme/env'))
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   // experimental: {
   //   appDir: false
@@ -20,6 +27,9 @@ const nextConfig = {
     '@acme/auth',
     '@acme/env',
   ],
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../')
+  }
 }
 
 export default nextConfig
